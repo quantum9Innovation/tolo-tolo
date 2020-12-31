de7377ed360f971981d781e1baad440b81e92985
quantum9Innovation <46249765+quantum9Innovation@users.noreply.github.com>
2020-12-30 19:16:12 -0800

    Moved Romanization Tables to Root Directory

---
+++ fidal.js
@@ -0,0 +1,36 @@
+fidal = 'ሀ ha ሁ hu ሂ hi ሃ hā ሄ hé ህ he ሆ ho \
+ለ la ሉ lu ሊ li ላ lā ሌ lé ል le ሎ lo \
+ሐ ḥa ሑ ḥu ሒ ḥi ሓ ḥā ሔ ḥé ሕ ḥe ሖ ḥo \
+መ ma ሙ mu ሚ mi ማ mā ሜ mé ም me ሞ mo \
+ሠ śa ሡ śu ሢ śi ሣ śā ሤ śé ሥ śe ሦ śo \
+ረ ra ሩ ru ሪ ri ራ rā ሬ ré ር re ሮ ro \
+ሰ sa ሱ su ሲ si ሳ sā ሴ sé ስ se ሶ so \
+ሸ ša ሹ šu ሺ ši ሻ šā ሼ šé ሽ še ሾ šo \
+ቀ qa ቁ qu ቂ qi ቃ qā ቄ qé ቅ qe ቆ qo \
+በ ba ቡ bu ቢ bi ባ bā ቤ bé ብ be ቦ bo \
+ተ ta ቱ tu ቲ ti ታ tā ቴ té ት te ቶ to \
+ቸ ča ቹ ču ቺ či ቻ čā ቼ čé ች če ቾ čo \
+ኀ Ha ኁ Hu ኂ Hi ኃ Hā ኄ Hé ኅ He ኆ Ho \
+ነ na ኑ nu ኒ ni ና nā ኔ né ን ne ኖ no \
+ኘ ña ኙ ñu ኚ ñi ኛ ñā ኜ ñé ኝ ñe ኞ ño \
+አ ʼa ኡ ʼu ኢ ʼi ኣ ʼā ኤ ʼé እ ʼe ኦ ʼo \
+ከ ka ኩ ku ኪ ki ካ kā ኬ ké ክ ke ኮ ko \
+ኸ xa ኹ xu ኺ xi ኻ xā ኼ xé ኽ xe ኾ xo \
+ወ wa ዉ wu ዊ wi ዋ wā ዌ wé ው we ዎ wo \
+ዐ ~a ዑ ~u ዒ ~i ዓ ~ā ዔ ~é ዕ ~e ዖ ~o \
+ዘ za ዙ zu ዚ zi ዛ zā ዜ zé ዝ ze ዞ zo \
+ዠ ža ዡ žu ዢ ži ዣ žā ዤ žé ዥ,ዥ že ዦ žo \
+የ ya ዩ yu ዪ yi ያ yā ዬ yé ይ ye ዮ yo \
+ደ da ዱ du ዲ di ዳ dā ዴ dé ድ de ዶ do \
+ጀ ja ጁ ju ጂ ji ጃ jā ጄ jé ጅ je ጆ jo \
+ገ ga ጉ gu ጊ gi ጋ gā ጌ gé ግ ge ጎ go \
+ጠ ṭa ጡ ṭu ጢ ṭi ጣ ṭā ጤ ṭé ጥ ṭe ጦ ṭo \
+ጨ ċa ጩ ċu ጪ ċi ጫ ċā ጬ ċé ጭ ċe ጮ ċo \
+ጰ p̣a ጱ p̣u ጲ p̣i ጳ p̣ā ጴ p̣é ጵ p̣e ጶ p̣o \
+ጸ ṣa ጹ ṣu ጺ ṣi ጻ ṣā ጼ ṣé ጽ ṣe ጾ ṣo \
+ፀ ṡa ፁ ṡu ፂ ṡi ፃ ṡā ፄ ṡé ፅ ṡe ፆ ṡo \
+ፈ fa ፉ fu ፊ fi ፋ fā ፌ fé ፍ fe ፎ fo \
+ፐ pa ፑ pu ፒ pi ፓ pā ፔ pé ፕ pe ፖ po \
+ቨ va ቩ vu ቪ vi ቫ vā ቬ vé ቭ ve ቮ vo'
+
+fidal = fidal.split(' ')
