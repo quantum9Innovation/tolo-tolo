b9e834e120b30032ee318e4aac6dc635d6369035
quantum9Innovation <46249765+quantum9Innovation@users.noreply.github.com>
2021-02-27 20:19:05 -0800

    Added Transliteration Scheme to Fix #7  - Input Processing on verbs/lookup #7  Use dashes (-) to seperate characters, see input-fidal.js for English  characters to match Amharic characters (case-sensitive)

---
+++ verbs/lookup/input-fidal.js
@@ -0,0 +1,36 @@
+var transliterator = 'ሀ he ሁ hu ሂ hee ሃ ha ሄ hay ህ hi ሆ ho \
+ለ le ሉ lu ሊ lee ላ la ሌ lay ል li ሎ lo \
+ሐ hhe ሑ hhu ሒ hhee ሓ hha ሔ hhay ሕ hhi ሖ hho \
+መ me ሙ mu ሚ mee ማ ma ሜ may ም mi ሞ mo \
+ሠ sse ሡ ssu ሢ ssee ሣ ssa ሤ ssay ሥ ssi ሦ sso \
+ረ re ሩ ru ሪ ree ራ ra ሬ ray ር ri ሮ ro \
+ሰ se ሱ su ሲ see ሳ sa ሴ say ስ si ሶ so \
+ሸ she ሹ shu ሺ shee ሻ sha ሼ shay ሽ shi ሾ sho \
+ቀ qe ቁ qu ቂ qee ቃ qa ቄ qay ቅ qi ቆ qo \
+በ be ቡ bu ቢ bee ባ ba ቤ bay ብ bi ቦ bo \
+ተ te ቱ tu ቲ tee ታ ta ቴ tay ት ti ቶ to \
+ቸ che ቹ chu ቺ chee ቻ cha ቼ chay ች chi ቾ cho \
+ኀ He ኁ Hu ኂ Hee ኃ Ha ኄ Hay ኅ Hi ኆ Ho \
+ነ ne ኑ nu ኒ nee ና na ኔ nay ን ni ኖ no \
+ኘ nye ኙ nyu ኚ nyee ኛ nya ኜ nyay ኝ nyi ኞ nyo \
+አ e ኡ u ኢ ee ኣ a ኤ ay እ i ኦ o \
+ከ ke ኩ ku ኪ kee ካ ka ኬ kay ክ ki ኮ ko \
+ኸ Hhe ኹ Hhu ኺ Hhee ኻ Hha ኼ Hhay ኽ Hhi ኾ Hho \
+ወ we ዉ wu ዊ wee ዋ wa ዌ way ው wi ዎ wo \
+ዐ ~e ዑ ~u ዒ ~ee ዓ ~a ዔ ~ay ዕ ~i ዖ ~o \
+ዘ ze ዙ zu ዚ zee ዛ za ዜ zay ዝ zi ዞ zo \
+ዠ jje ዡ jju ዢ jjee ዣ jja ዤ jjay ዥ,ዥ jji ዦ jjo \
+የ ye ዩ yu ዪ yee ያ ya ዬ yay ይ yi ዮ yo \
+ደ de ዱ du ዲ dee ዳ da ዴ day ድ di ዶ do \
+ጀ je ጁ ju ጂ jee ጃ ja ጄ jay ጅ ji ጆ jo \
+ገ ge ጉ gu ጊ gee ጋ ga ጌ gay ግ gi ጎ go \
+ጠ Te ጡ Tu ጢ Tee ጣ Ta ጤ Tay ጥ Ti ጦ To \
+ጨ Che ጩ Chu ጪ Chee ጫ Cha ጬ Chay ጭ Chi ጮ Cho \
+ጰ Pe ጱ Pu ጲ Pee ጳ Pa ጴ Pay ጵ Pi ጶ Po \
+ጸ Se ጹ Su ጺ See ጻ Sa ጼ Say ጽ Si ጾ So \
+ፀ Sse ፁ Ssu ፂ Ssee ፃ Ssa ፄ Ssay ፅ Ssi ፆ Sso \
+ፈ fe ፉ fu ፊ fee ፋ fa ፌ fay ፍ fi ፎ fo \
+ፐ pe ፑ pu ፒ pee ፓ pa ፔ pay ፕ pi ፖ po \
+ቨ ve ቩ vu ቪ vee ቫ va ቬ vay ቭ vi ቮ vo'
+
+transliterator = transliterator.split(' ')
